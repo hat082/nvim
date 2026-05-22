21a817ee2b72505af9e1838d048f342bd4764e19
jack <something@email.com>
2026-05-22 11:05:56 +0100

    ftplugin for md files

---
+++ ftplugin/markdown.lua
@@ -0,0 +1,2 @@
+vim.keymap.set('n', 'j', 'gj', { buffer = true, silent = true, desc = 'Move down by display line' })
+vim.keymap.set('n', 'k', 'gk', { buffer = true, silent = true, desc = 'Move up by display line' })
