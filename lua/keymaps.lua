711014330aa6a90b52ea50530609c2aa71aec5c8
jack <something@email.com>
2026-05-22 11:06:08 +0100

    vim tmux nav

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -70,10 +70,10 @@ end
 -- ============================================================
 -- [[ Window Navigation & Resize ]]
 -- ============================================================
-map('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus left' })
-map('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus down' })
-map('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus up' })
-map('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus right' })
+map('n', '<C-h>', '<cmd>TmuxNavigateLeft<CR>', { desc = 'Move focus left' })
+map('n', '<C-j>', '<cmd>TmuxNavigateDown<CR>', { desc = 'Move focus down' })
+map('n', '<C-k>', '<cmd>TmuxNavigateUp<CR>', { desc = 'Move focus up' })
+map('n', '<C-l>', '<cmd>TmuxNavigateRight<CR>', { desc = 'Move focus right' })
 map('n', '<C-Up>', ':resize -2<CR>', opts)
 map('n', '<C-Down>', ':resize +2<CR>', opts)
 map('n', '<C-Left>', ':vertical resize -2<CR>', opts)
@@ -121,7 +121,7 @@ end, { desc = 'Click URL to open in browser' })
 -- ============================================================
 -- [[ Diagnostics ]]
 -- ============================================================
-map('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+map('n', '<leader>lq', vim.diagnostic.setqflist, { desc = 'Open [L]SP diagnostics [Q]uickfix list' })
 
 -- ============================================================
 -- [[ Terminal ]]
@@ -166,6 +166,9 @@ end, { desc = '[Y]ank fenced code block to clipboard' })
 -- ============================================================
 do
   if ok_snacks then
+    map('n', '<leader><space>', function()
+      snacks.picker.files()
+    end, { desc = '[F]ind [F]iles' })
     map('n', '<leader>ff', function()
       snacks.picker.files()
     end, { desc = '[F]ind [F]iles' })
@@ -248,6 +251,11 @@ map('n', '<leader>gg', '<cmd>LazyGit<CR>', { desc = '[G]it lazy[G]it' })
 map('n', '<S-l>', '<cmd>BufferLineCycleNext<CR>', { desc = 'Next buffer' })
 map('n', '<S-h>', '<cmd>BufferLineCyclePrev<CR>', { desc = 'Prev buffer' })
 map('n', '<leader>d', '<cmd>bdelete<CR>', { desc = 'Close buffer' })
+map('n', '<leader>xe', vim.diagnostic.open_float, { desc = 'Line diagnostics' })
+map('n', '<leader>xp', vim.diagnostic.goto_prev, { desc = 'Previous diagnostic' })
+map('n', '<leader>xn', vim.diagnostic.goto_next, { desc = 'Next diagnostic' })
+map('n', '<leader>xq', '<cmd>copen<CR>', { desc = 'Open quickfix list' })
+map('n', '<leader>xl', '<cmd>lopen<CR>', { desc = 'Open location list' })
 map('n', '<leader>xx', '<cmd>Trouble diagnostics toggle<CR>', { desc = 'Diagnostics (Trouble)' })
 map('n', '<leader>xb', '<cmd>Trouble diagnostics toggle filter.buf=0<CR>', { desc = 'Buffer Diagnostics' })
 map('n', '<leader>xs', '<cmd>Trouble symbols toggle<CR>', { desc = 'Symbols (Trouble)' })
@@ -273,8 +281,8 @@ local function toggle_terminal()
   vim.cmd('ToggleTerm dir=' .. vim.fn.fnameescape(dir))
 end
 
-map('n', '<C-_>', toggle_terminal, { desc = 'Toggle terminal (buffer directory)' })
-map('n', '<C-/>', toggle_terminal, { desc = 'Toggle terminal (buffer directory)' })
+map({ 'n', 't' }, '<C-_>', toggle_terminal, { desc = 'Toggle terminal (buffer directory)' })
+map({ 'n', 't' }, '<C-/>', toggle_terminal, { desc = 'Toggle terminal (buffer directory)' })
 
 -- ============================================================
 -- [[ LSP ]]
@@ -297,6 +305,18 @@ vim.api.nvim_create_autocmd('LspAttach', {
     map_lsp('<leader>la', vim.lsp.buf.code_action, '[L]SP code [A]ction', { 'n', 'x' })
     map_lsp('<leader>lk', hover_in_vsplit, '[L]SP hover in vertical split')
     if ok_snacks then
+      map_lsp('gr', function()
+        snacks.picker.lsp_references()
+      end, '[G]oto [R]eferences')
+      map_lsp('gI', function()
+        snacks.picker.lsp_implementations()
+      end, '[G]oto [I]mplementation')
+      map_lsp('gd', function()
+        snacks.picker.lsp_definitions()
+      end, '[G]oto [D]efinition')
+      map_lsp('gy', function()
+        snacks.picker.lsp_type_definitions()
+      end, '[G]oto t[Y]pe definition')
       map_lsp('<leader>lR', function()
         snacks.picker.lsp_references()
       end, '[L]SP [R]eferences')
@@ -315,7 +335,13 @@ vim.api.nvim_create_autocmd('LspAttach', {
       map_lsp('<leader>lt', function()
         snacks.picker.lsp_type_definitions()
       end, '[L]SP [T]ype definition')
+    else
+      map_lsp('gr', vim.lsp.buf.references, '[G]oto [R]eferences')
+      map_lsp('gI', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
+      map_lsp('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
+      map_lsp('gy', vim.lsp.buf.type_definition, '[G]oto t[Y]pe definition')
     end
+    map_lsp('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
     map_lsp('<leader>lD', vim.lsp.buf.declaration, '[L]SP [D]eclaration')
 
     local client = vim.lsp.get_client_by_id(event.data.client_id)
@@ -366,11 +392,31 @@ map('n', '<leader>br', function()
 end, { desc = '[B]uild and [R]un current C/C++ file' })
 
 -- ============================================================
+-- [[ Python ]]
+-- ============================================================
+map('n', '<F5>', function()
+  local ext = vim.fn.expand '%:e'
+  if ext ~= 'py' then
+    vim.notify('Not a Python file', vim.log.levels.WARN)
+    return
+  end
+  local fpath = vim.fn.fnameescape(vim.fn.expand '%:p')
+  local cmd
+  -- Prefer uv run if in a project with pyproject.toml or uv.lock
+  if vim.fn.filereadable 'pyproject.toml' == 1 or vim.fn.filereadable 'uv.lock' == 1 then
+    cmd = 'uv run python ' .. fpath
+  else
+    cmd = 'python3 ' .. fpath
+  end
+  vim.cmd('ToggleTerm cmd=' .. cmd)
+end, { desc = 'Run Python file' })
+
+-- ============================================================
 -- [[ Tools & Plugins ]]
 -- ============================================================
 map('n', '<leader>a', ':Alpha<CR>', { silent = true, desc = 'Open [A]lpha dashboard' })
 map('n', '<leader>p', '<cmd>PasteImage<CR>', { silent = true, desc = '[P]aste image from clipboard' })
-map('n', '<F5>', '<cmd>UndotreeToggle<CR><cmd>UndotreeFocus<CR>', { silent = true, desc = 'Toggle Undotree' })
+map('n', '<F6>', '<cmd>UndotreeToggle<CR><cmd>UndotreeFocus<CR>', { silent = true, desc = 'Toggle Undotree' })
 
 -- ============================================================
 -- [[ OpenCode ]]
